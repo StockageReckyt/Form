ad76ae2c16459175655e38889a85b7da35717fdd
Maxime <maxime.lombardo@gmail.com>
2019-11-17 16:37:49 +0100

    ça marche pas encore

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
 
 import "./App.css";
-import FormEmployee from "./components/FormEmployee";
-
-const config = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify()
-};
-
-const url = "https://post-a-form.herokuapp.com/api/movies/";
-
-fetch(url, config)
-  .then(res => res.json())
-  .then(res => {
-    if (res.error) {
-      alert(res.error);
-    } else {
-      alert(`Film ajouté avec l'ID ${res}!`);
-    }
-  })
-  .catch(e => {
-    console.error(e);
-    alert("Erreur lors de l'ajout d'un employé");
-  });
+import FavoriteMovie from "./components/FavoriteMovie";
 
 function App() {
   return (
     <div className="App">
-      <FormEmployee />
+      <FavoriteMovie />
     </div>
   );
 }
---
+++ src/components/FavoriteMovie.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+
+import "./FavoriteMovie.css";
+
+export default class FavoriteMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      poster: "",
+      comments: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    };
+
+    const url = "https://post-a-form.herokuapp.com/api/movies/";
+
+    fetch(url, config)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`Movie add with the ID ${res}!`);
+        }
+      })
+      .catch(e => {
+        console.error(e);
+        alert("Erratum");
+      });
+  }
+
+  render() {
+    return (
+      <div className="FavoriteMovie">
+        <h1>Favorite movie</h1>
+
+        <form onSubmit={this.submitForm}>
+          <fieldset>
+            <legend>Movie</legend>
+            <div className="form-data">
+              <label htmlFor="title">Title</label>
+              <input
+                required
+                type="text"
+                id="title"
+                name="title"
+                onChange={this.onChange}
+                value={this.state.title}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="poster">Poster URL</label>
+              <input
+                required
+                type="text"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                value={this.state.poster}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="comments">Comments</label>
+              <textarea
+                required
+                id="comments"
+                name="comments"
+                onChange={this.onChange}
+                value={this.state.comments}
+              />
+            </div>
+            <hr />
+            <div className="form-data">
+              <input type="submit" value="Send" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
